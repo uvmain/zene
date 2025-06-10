275f11ca0e828ed66344a20d897d974d16dcb706
Ian Baron <ian.baron@manchester.ac.uk>
2025-06-10 16:21:28 +0100

    add volume slider

--- frontend/src/components/FooterPlayer.vue
+++ frontend/src/components/FooterPlayer.vue
@@ -7,6 +7,7 @@ import { getRandomTrack } from '../composables/randomTrack'
 const audioRef = ref<HTMLAudioElement | null>(null)
 const isPlaying = ref(false)
 const currentTime = ref(0)
+const currentVolume = ref(1)
 const isPlayPauseActive = ref(false)
 const debug = ref()
 
@@ -67,6 +68,15 @@ function seek(event: Event) {
   const target = event.target as HTMLInputElement
   const seekTime = Number.parseFloat(target.value)
   audioRef.value.currentTime = seekTime
+}
+
+function volumeInput(event: Event) {
+  if (!audioRef.value)
+    return
+  const target = event.target as HTMLInputElement
+  const volume = Number.parseFloat(target.value)
+  audioRef.value.volume = volume
+  currentVolume.value = volume
 }
 
 async function randomTrack() {
@@ -174,65 +184,80 @@ onUnmounted(() => {
 <template>
   <footer
     class="sticky bottom-0 mt-auto w-full bg-zene-700 bg-cover bg-center"
-    :style="{ backgroundImage: `url(${currentlyPlayingTrack?.image_url})` }"
     :class="{ 'animate-pulse-bg': currentlyPlayingTrack && isPlaying }"
+    :style="{ backgroundImage: `url(${currentlyPlayingTrack?.image_url})` }"
   >
-    <div
-      class="mb-8 h-full w-full flex flex-grow flex-col items-center justify-center bg-zene-700 backdrop-blur-xl backdrop-contrast-50 space-y-2"
-    >
-      <audio ref="audioRef" :src="trackUrl" preload="metadata" class="hidden" />
-      <div class="">
-        <!-- Progress Bar -->
-        <div v-if="audioRef" class="max-w-200 flex flex-row items-center gap-2">
-          <span id="currentTime" class="w-12 text-right text-sm text-gray-2">
-            {{ formatTime(currentTime) }}
-          </span>
-          <input
-            type="range"
-            class="h-1 w-full cursor-pointer bg-white/60 accent-zene-200"
-            :max="currentlyPlayingTrack ? currentlyPlayingTrack.duration : 0"
-            :value="currentTime"
-            @input="seek"
-          />
-          <span id="duration" class="w-12 text-sm text-gray-2">
-            {{ formatTime(currentlyPlayingTrack ? Number.parseFloat(currentlyPlayingTrack.duration) : 0) }}
-          </span>
-        </div>
+    <div class="flex flex-row items-center px-4 backdrop-blur-xl backdrop-contrast-50 space-x-2">
+      <div
+        class="mb-8 h-full w-full flex flex-grow flex-col items-center justify-center space-y-2"
+      >
+        <audio ref="audioRef" :src="trackUrl" preload="metadata" class="hidden" />
+        <div class="">
+          <!-- Progress Bar -->
+          <div v-if="audioRef" class="max-w-200 flex flex-row items-center gap-2">
+            <span id="currentTime" class="w-12 text-right text-sm text-gray-2">
+              {{ formatTime(currentTime) }}
+            </span>
+            <input
+              type="range"
+              class="h-1 w-full cursor-pointer bg-white/60 accent-zene-200"
+              :max="currentlyPlayingTrack ? currentlyPlayingTrack.duration : 0"
+              :value="currentTime"
+              @input="seek"
+            />
+            <span id="duration" class="w-12 text-sm text-gray-2">
+              {{ formatTime(currentlyPlayingTrack ? Number.parseFloat(currentlyPlayingTrack.duration) : 0) }}
+            </span>
+          </div>
 
-        <!-- Buttons -->
-        <div class="mt-2 flex flex-row items-center justify-center gap-x-4">
-          <button id="repeat" class="h-12 w-12 flex cursor-pointer items-center justify-center rounded-full border-none bg-zene-400/0 text-white font-semibold outline-none" @click="stopPlayback()">
-            <icon-tabler-player-stop class="text-xl" />
-          </button>
-          <button id="shuffle" class="h-12 w-12 flex cursor-pointer items-center justify-center rounded-full border-none bg-zene-400/0 text-white font-semibold outline-none" @click="togglePlayback()">
-            <icon-tabler-arrows-shuffle class="text-xl" />
-          </button>
-          <button id="back" class="h-12 w-12 flex cursor-pointer items-center justify-center rounded-full border-none bg-zene-400/0 text-white font-semibold outline-none" @click="togglePlayback()">
-            <icon-tabler-player-skip-back class="text-xl" />
-          </button>
-          <button
-            id="play-pause"
-            class="h-12 w-12 flex cursor-pointer items-center justify-center rounded-md border-none text-white font-semibold outline-none"
-            :class="isPlayPauseActive ? 'bg-zene-200' : 'bg-zene-400 transition-colors duration-200'"
-            @click="togglePlayback()"
-          >
-            <icon-tabler-player-play v-if="!isPlaying" class="text-3xl" />
-            <icon-tabler-player-pause v-else class="text-3xl" />
-          </button>
-          <button id="forward" class="h-12 w-12 flex cursor-pointer items-center justify-center rounded-full border-none bg-zene-400/0 text-white font-semibold outline-none" @click="playNext()">
-            <icon-tabler-player-skip-forward class="text-xl" />
-          </button>
-          <button id="repeat" class="h-12 w-12 flex cursor-pointer items-center justify-center rounded-full border-none bg-zene-400/0 text-white font-semibold outline-none" @click="togglePlayback()">
-            <icon-tabler-repeat class="text-xl" />
-          </button>
-          <button
-            id="shuffle"
-            class="h-12 w-12 flex cursor-pointer items-center justify-center rounded-full border-none bg-zene-400/0 text-white font-semibold outline-none"
-            @click="randomTrack()"
-          >
-            <icon-tabler-dice-3 class="text-xl" />
-          </button>
+          <!-- Buttons -->
+          <div class="mt-2 flex flex-row items-center justify-center gap-x-4">
+            <button id="repeat" class="h-12 w-12 flex cursor-pointer items-center justify-center rounded-full border-none bg-zene-400/0 text-white font-semibold outline-none" @click="stopPlayback()">
+              <icon-tabler-player-stop class="text-xl" />
+            </button>
+            <button id="shuffle" class="h-12 w-12 flex cursor-pointer items-center justify-center rounded-full border-none bg-zene-400/0 text-white font-semibold outline-none" @click="togglePlayback()">
+              <icon-tabler-arrows-shuffle class="text-xl" />
+            </button>
+            <button id="back" class="h-12 w-12 flex cursor-pointer items-center justify-center rounded-full border-none bg-zene-400/0 text-white font-semibold outline-none" @click="togglePlayback()">
+              <icon-tabler-player-skip-back class="text-xl" />
+            </button>
+            <button
+              id="play-pause"
+              class="h-12 w-12 flex cursor-pointer items-center justify-center rounded-md border-none text-white font-semibold outline-none"
+              :class="isPlayPauseActive ? 'bg-zene-200' : 'bg-zene-400 transition-colors duration-200'"
+              @click="togglePlayback()"
+            >
+              <icon-tabler-player-play v-if="!isPlaying" class="text-3xl" />
+              <icon-tabler-player-pause v-else class="text-3xl" />
+            </button>
+            <button id="forward" class="h-12 w-12 flex cursor-pointer items-center justify-center rounded-full border-none bg-zene-400/0 text-white font-semibold outline-none" @click="playNext()">
+              <icon-tabler-player-skip-forward class="text-xl" />
+            </button>
+            <button id="repeat" class="h-12 w-12 flex cursor-pointer items-center justify-center rounded-full border-none bg-zene-400/0 text-white font-semibold outline-none" @click="togglePlayback()">
+              <icon-tabler-repeat class="text-xl" />
+            </button>
+            <button
+              id="shuffle"
+              class="h-12 w-12 flex cursor-pointer items-center justify-center rounded-full border-none bg-zene-400/0 text-white font-semibold outline-none"
+              @click="randomTrack()"
+            >
+              <icon-tabler-dice-3 class="text-xl" />
+            </button>
+          </div>
         </div>
+      </div>
+      <div id="volume-range-input" class="flex flex-row items-center gap-2">
+        <icon-tabler-volume v-if="currentVolume > 0.5" class="text-sm" />
+        <icon-tabler-volume-2 v-else-if="currentVolume > 0" class="text-sm" />
+        <icon-tabler-volume-3 v-else class="text-sm" />
+        <input
+          type="range"
+          class="h-1 w-30 cursor-pointer bg-white/60 accent-zene-200"
+          max="1"
+          step="0.01"
+          :value="currentVolume"
+          @input="volumeInput"
+        />
       </div>
     </div>
   </footer>
