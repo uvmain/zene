589010b481b61fd40d1130da828504b2c3ad6f76
Ian Baron <ian.baron@manchester.ac.uk>
2025-06-17 11:18:46 +0100

    add HandleUpsertPlaycount route

--- core/handlers/playcount_handlers.go
+++ core/handlers/playcount_handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"strconv"
+	"zene/core/auth"
 	"zene/core/database"
 	"zene/core/types"
 )
@@ -26,6 +27,32 @@ func HandleGetPlaycounts(w http.ResponseWriter, r *http.Request) {
 	response.Playcounts = make([]*types.Playcount, len(rows))
 	for i := range rows {
 		response.Playcounts[i] = &rows[i]
+	}
+
+	handleSuccessResponse(w, response)
+	return
+}
+
+func HandleUpsertPlaycount(w http.ResponseWriter, r *http.Request) {
+	userIdParam := r.URL.Query().Get("user_id")
+	musicbrainzTrackId := r.URL.Query().Get("musicbrainz_track_id")
+
+	response := &StandardResponse{}
+
+	userId, err := strconv.ParseInt(userIdParam, 10, 64)
+	if err != nil {
+		user, _, err := auth.GetUserFromRequest(r)
+		if err != nil {
+			handleErrorResponse(w, response, "Failed to get user_id", err, http.StatusInternalServerError)
+		} else {
+			userId = user.Id
+		}
+	}
+
+	err = database.UpsertPlaycount(r.Context(), userId, musicbrainzTrackId)
+	if err != nil {
+		handleErrorResponse(w, response, "Failed to upsert playcount", err, http.StatusInternalServerError)
+		return
 	}
 
 	handleSuccessResponse(w, response)
