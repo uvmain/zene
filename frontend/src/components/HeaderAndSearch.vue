5bbbfd54072db2bcafa46b4f82644f9c84adb26a
Ian Baron <ian.baron@manchester.ac.uk>
2025-07-13 11:46:30 +0100

    fix search bar overflow

--- frontend/src/components/HeaderAndSearch.vue
+++ frontend/src/components/HeaderAndSearch.vue
@@ -12,15 +12,15 @@ const toggleDark = useToggle(isDark)
 
 <template>
   <header>
-    <div class="flex gap-2 md:gap-4 p-2">
+    <div class="flex gap-2 p-2 md:gap-4">
       <!-- Mobile hamburger menu -->
-      <div class="flex md:hidden">
+      <div class="flex items-center justify-center md:hidden">
         <icon-tabler-menu-2 class="text-2xl" @click="toggleMobileNav()" />
       </div>
 
-      <div class="flex flex-grow justify-center">
-        <div class="relative w-full max-w-xs md:max-w-md md:w-1/2">
-          <span class="absolute inset-y-0 left-0 flex justify-center pl-3 text-gray-400">
+      <div class="flex flex-grow items-center justify-center">
+        <div class="relative max-w-xs w-full md:max-w-md md:w-1/2">
+          <span class="absolute inset-y-0 left-0 h-full flex items-center justify-center pl-3 text-gray-400">
             <icon-tabler-search class="text-xl" />
           </span>
           <input
@@ -28,7 +28,7 @@ const toggleDark = useToggle(isDark)
             v-model="searchInput"
             placeholder="Type here to search"
             type="text"
-            class="block w-full border border-zene-400 rounded-lg bg-gray-800 px-10 py-2 text-white focus:border-zene-200 focus:border-solid focus:shadow-zene-400 hover:shadow-lg focus:outline-none"
+            class="border border-zene-400 rounded-lg bg-gray-800 py-2 pl-10 text-white focus:border-zene-200 focus:border-solid focus:shadow-zene-400 hover:shadow-lg focus:outline-none"
             @change="search()"
             @input="search()"
             @keydown.escape="searchInput = ''"
@@ -36,12 +36,12 @@ const toggleDark = useToggle(isDark)
         </div>
       </div>
       <div id="user-and-settings" class="flex gap-4">
-        <div class="hover:cursor-pointer" @click="toggleDark()">
+        <div class="items-center justify-center hover:cursor-pointer" @click="toggleDark()">
           <icon-tabler-sun v-if="isDark" class="text-2xl" />
           <icon-tabler-moon-stars v-else class="text-2xl" />
         </div>
-        <SettingsDropDown />
-        <div class="hover:cursor-pointer">
+        <SettingsDropDown class="items-center justify-center" />
+        <div class="items-center justify-center hover:cursor-pointer">
           <icon-tabler-user class="text-2xl" />
         </div>
       </div>
