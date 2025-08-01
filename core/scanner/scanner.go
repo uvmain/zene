85faecfcbe6574df8bf0685f04725126452c55fa
Ian Baron <ian.baron@manchester.ac.uk>
2025-08-01 09:14:25 +0100

    add debug logging for scanner upserts

--- core/scanner/scanner.go
+++ core/scanner/scanner.go
@@ -58,6 +58,7 @@ func RunScan(ctx context.Context) types.ScanResponse {
 		if err != nil {
 			scanError("Error inserting or updating metadata row: %v", err)
 		} else {
+			logger.Printf("Scan: Upserted metadata for %s", audioFile.FilePathAbs)
 			fileCount += 1
 		}
 	}
