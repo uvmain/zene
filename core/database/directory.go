5b1d7b1d1f35f526de9afe8a09d494ba1c18002d
Ian Baron <ian.baron@manchester.ac.uk>
2025-08-19 10:08:01 +0100

    getAlbumDirectory uses albumId not artistId

--- core/database/directory.go
+++ core/database/directory.go
@@ -16,8 +16,7 @@ func GetAlbumDirectory(ctx context.Context, musicbrainzAlbumId string) (types.Di
 		return types.Directory{}, err
 	}
 
-	query := `SELECT 
-		m.musicbrainz_artist_id AS parent,
+	query := `SELECT m.musicbrainz_artist_id AS parent,
 		m.album AS name,
 		s.created_at AS starred,
 		COALESCE(ur.rating, 0) AS user_rating,
@@ -26,12 +25,12 @@ func GetAlbumDirectory(ctx context.Context, musicbrainzAlbumId string) (types.Di
 		COUNT(m.musicbrainz_track_id) AS song_count
 	FROM metadata m
 	JOIN user_music_folders f ON f.folder_id = m.music_folder_id AND f.user_id = 1
-	LEFT JOIN user_stars s ON m.musicbrainz_artist_id = s.metadata_id AND s.user_id = f.user_id
-	LEFT JOIN user_ratings ur ON m.musicbrainz_artist_id = ur.metadata_id AND ur.user_id = f.user_id
-	LEFT JOIN user_ratings gr ON m.musicbrainz_artist_id = gr.metadata_id
+	LEFT JOIN user_stars s ON m.musicbrainz_album_id = s.metadata_id AND s.user_id = f.user_id
+	LEFT JOIN user_ratings ur ON m.musicbrainz_album_id = ur.metadata_id AND ur.user_id = f.user_id
+	LEFT JOIN user_ratings gr ON m.musicbrainz_album_id = gr.metadata_id
 	LEFT JOIN play_counts pc ON m.musicbrainz_track_id = pc.musicbrainz_track_id AND pc.user_id = f.user_id
 	WHERE f.user_id = ? and m.musicbrainz_album_id = ?
-	GROUP BY m.musicbrainz_artist_id, m.album, s.created_at, ur.rating;;`
+	GROUP BY m.musicbrainz_album_id, m.album, s.created_at, ur.rating;`
 
 	var starred sql.NullString
 
