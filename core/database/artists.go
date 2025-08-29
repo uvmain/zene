2ceb8104927278873fa9fd0078ce10906b47c9ed
Ian Baron <ian.baron@manchester.ac.uk>
2025-08-29 17:14:48 +0100

    fix missing albumCount in getIndexes

--- core/database/artists.go
+++ core/database/artists.go
@@ -15,7 +15,7 @@ func SelectArtistByMusicBrainzArtistId(ctx context.Context, musicbrainzArtistId 
 	}
 	query := `SELECT m.musicbrainz_artist_id as id,
 		m.artist as name,
-		count(distinct m.musicbrainz_album_id) as album_count,
+		coalesce(count(distinct m.musicbrainz_album_id), 0) as album_count,
 		s.created_at as starred,
 		COALESCE(ur.rating, 0) AS user_rating,
 		COALESCE(AVG(gr.rating),0.0) AS average_rating
--- core/database/indexes.go
+++ core/database/indexes.go
@@ -41,13 +41,14 @@ func getArtistIndexes(ctx context.Context, userId int, musicFolderIds []int) ([]
 	var err error
 
 	query := `SELECT case when substr(m.artist,1,1) GLOB '*[0-9]*' then '#' else upper(substr(m.artist,1,1)) end as artist_index,
-		m.musicbrainz_artist_id, m.artist, s.created_at, COALESCE(ur.rating, 0) AS user_rating, COALESCE(AVG(gr.rating),0.0) AS average_rating
+		m.musicbrainz_artist_id, m.artist, s.created_at, COALESCE(ur.rating, 0) AS user_rating, COALESCE(AVG(gr.rating),0.0) AS average_rating,
+		coalesce(count(distinct m.album), 0) as album_count
 		FROM metadata m
 		LEFT JOIN user_stars s ON m.musicbrainz_artist_id = s.metadata_id AND s.user_id = ?
 		LEFT JOIN user_ratings ur ON m.musicbrainz_artist_id = ur.metadata_id AND ur.user_id = ?
 		LEFT JOIN user_ratings gr ON m.musicbrainz_artist_id = gr.metadata_id`
-
 	args := []any{userId, userId}
+
 	if len(musicFolderIds) > 0 {
 		placeholders := make([]string, len(musicFolderIds))
 		for i, id := range musicFolderIds {
@@ -73,7 +74,7 @@ func getArtistIndexes(ctx context.Context, userId int, musicFolderIds []int) ([]
 		var artist types.Artist
 		var artistIndex string
 		var starred sql.NullString
-		if err := rows.Scan(&artistIndex, &artist.Id, &artist.Name, &starred, &artist.UserRating, &artist.AverageRating); err != nil {
+		if err := rows.Scan(&artistIndex, &artist.Id, &artist.Name, &starred, &artist.UserRating, &artist.AverageRating, &artist.AlbumCount); err != nil {
 			return nil, fmt.Errorf("scanning artist row: %v", err)
 		}
 		if starred.Valid {
@@ -87,6 +88,7 @@ func getArtistIndexes(ctx context.Context, userId int, musicFolderIds []int) ([]
 			Name:           artist.Name,
 			CoverArt:       artist.Id,
 			ArtistImageUrl: artist.ArtistImageUrl,
+			AlbumCount:     artist.AlbumCount,
 			Starred:        artist.Starred,
 			MusicBrainzId:  artist.Id,
 			SortName:       strings.ToLower(artist.Name),
