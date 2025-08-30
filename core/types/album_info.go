02a0d515d489ff3769532c80fa9769639d59159d
Ian Baron <ian.baron@manchester.ac.uk>
2025-08-30 13:30:51 +0100

    fix xml response for getAlbumInfo

--- core/types/album_info.go
+++ core/types/album_info.go
@@ -1,10 +1,10 @@
 package types
 
 type AlbumInfo struct {
-	Notes          string `json:"notes,omitempty" xml:"notes,attr,omitempty"`
-	MusicBrainzId  string `json:"musicBrainzId,omitempty" xml:"musicBrainzId,attr,omitempty"`
-	LastFmUrl      string `json:"lastFmUrl,omitempty" xml:"lastFmUrl,attr,omitempty"`
-	SmallImageUrl  string `json:"smallImageUrl,omitempty" xml:"smallImageUrl,attr,omitempty"`
-	MediumImageUrl string `json:"mediumImageUrl,omitempty" xml:"mediumImageUrl,attr,omitempty"`
-	LargeImageUrl  string `json:"largeImageUrl,omitempty" xml:"largeImageUrl,attr,omitempty"`
+	Notes          string `json:"notes,omitempty" xml:"notes,omitempty"`
+	MusicBrainzId  string `json:"musicBrainzId,omitempty" xml:"musicBrainzId,omitempty"`
+	LastFmUrl      string `json:"lastFmUrl,omitempty" xml:"lastFmUrl,omitempty"`
+	SmallImageUrl  string `json:"smallImageUrl,omitempty" xml:"smallImageUrl,omitempty"`
+	MediumImageUrl string `json:"mediumImageUrl,omitempty" xml:"mediumImageUrl,omitempty"`
+	LargeImageUrl  string `json:"largeImageUrl,omitempty" xml:"largeImageUrl,omitempty"`
 }
