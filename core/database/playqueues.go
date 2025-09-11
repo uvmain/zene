a0e5a4902244ac3722fdae0d7602e7a077046204
Ian Baron <uv.fragm@gmail.com>
2025-09-11 08:52:37 +0100

    add upsertPlayqueue method

--- core/database/playqueues.go
+++ core/database/playqueues.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"context"
+	"fmt"
+	"zene/core/logic"
 )
 
 func migratePlayqueues(ctx context.Context) {
@@ -18,4 +20,37 @@ func migratePlayqueues(ctx context.Context) {
 	);`
 	createTable(ctx, schema)
 	createIndex(ctx, "idx_playqueues_user", "playqueues", []string{"user_id"}, false)
+}
+
+func UpsertPlayqueue(ctx context.Context, trackIds []string, currentId string, position int, changedBy string) error {
+	user, err := GetUserByContext(ctx)
+	if err != nil {
+		return fmt.Errorf("getting user by context in UpsertPlayqueue")
+	}
+
+	trackIdsString := ""
+	for i, id := range trackIds {
+		if i > 0 {
+			trackIdsString += ","
+		}
+		trackIdsString += id
+	}
+
+	changed := logic.GetCurrentTimeFormatted()
+
+	query := `INSERT INTO playqueues (user_id, changed, changed_by, position, track_ids, current_id)
+		VALUES (?, ?, ?, ?, ?, ?)
+		ON CONFLICT(user_id) DO UPDATE SET
+			changed=excluded.changed,
+			changed_by=excluded.changed_by,
+			position=excluded.position,
+			track_ids=excluded.track_ids,
+			current_id=excluded.current_id
+		WHERE playqueues.user_id=excluded.user_id;`
+
+	_, err = DB.ExecContext(ctx, query, user.Id, changed, changedBy, position, trackIdsString, currentId)
+	if err != nil {
+		return err
+	}
+	return nil
 }
