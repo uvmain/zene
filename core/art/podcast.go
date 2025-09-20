6d92a278af2dbc54e1724bdf17750601526ae984
Ian Baron <ian.baron@manchester.ac.uk>
2025-09-20 15:25:22 +0100

    enable fetching coverArt for podcasts

---
+++ core/art/podcast.go
@@ -0,0 +1,29 @@
+package art
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+	"zene/core/config"
+	"zene/core/logic"
+)
+
+func GetArtForPodcast(ctx context.Context, coverArtId string, size int) ([]byte, time.Time, error) {
+	file_name := fmt.Sprintf("%s.jpg", coverArtId)
+	filePath, _ := filepath.Abs(filepath.Join(config.PodcastArtFolder, file_name))
+
+	info, err := os.Stat(filePath)
+	if err != nil {
+		return nil, time.Now(), fmt.Errorf("podcast coverArt file does not exist: %s:  %s", filePath, err)
+	}
+
+	modTime := info.ModTime()
+
+	blob, err := logic.ResizeJpegImage(ctx, filePath, size, 90)
+	if err != nil {
+		return nil, time.Now(), fmt.Errorf("error reading image for filepath %s: %s", filePath, err)
+	}
+	return blob, modTime, nil
+}
--- core/handlers/get_cover_art.go
+++ core/handlers/get_cover_art.go
@@ -40,38 +40,49 @@ func HandleGetCoverArt(w http.ResponseWriter, r *http.Request) {
 	}
 
 	valid, metadataStruct, err := database.IsValidMetadataId(ctx, idParameter)
-	if err != nil || !valid {
-		errorString := "invalid id parameter"
+	if err != nil && err.Error() != "checking metadata ID validity: sql: no rows in result set" {
+		errorString := "error checking coverArt id parameter"
+		logger.Printf("%s: %v", errorString, err)
 		net.WriteSubsonicError(w, r, types.ErrorMissingParameter, errorString, "")
 		return
 	}
 
 	var imageBlob []byte
 	var lastModified time.Time
-	if metadataStruct.MusicbrainzTrackId {
-		albumId, err := database.SelectAlbumIdByTrackId(ctx, idParameter)
-		if err != nil {
-			logger.Printf("Error getting album ID for track %s: %v", idParameter, err)
-			net.WriteSubsonicError(w, r, types.ErrorDataNotFound, "Album not found", "")
-			return
+
+	if valid {
+		if metadataStruct.MusicbrainzTrackId {
+			albumId, err := database.SelectAlbumIdByTrackId(ctx, idParameter)
+			if err != nil {
+				logger.Printf("Error getting album ID for track %s: %v", idParameter, err)
+				net.WriteSubsonicError(w, r, types.ErrorDataNotFound, "Album not found", "")
+				return
+			}
+			imageBlob, lastModified, err = art.GetArtForAlbum(ctx, albumId, sizeInt)
+			if err != nil {
+				logger.Printf("Error getting album cover art for %s: %v", idParameter, err)
+				net.WriteSubsonicError(w, r, types.ErrorDataNotFound, "Cover art not found", "")
+				return
+			}
+		} else if metadataStruct.MusicbrainzAlbumId {
+			imageBlob, lastModified, err = art.GetArtForAlbum(ctx, idParameter, sizeInt)
+			if err != nil {
+				logger.Printf("Error getting album cover art for %s: %v", idParameter, err)
+				net.WriteSubsonicError(w, r, types.ErrorDataNotFound, "Cover art not found", "")
+				return
+			}
+		} else if metadataStruct.MusicbrainzArtistId {
+			imageBlob, lastModified, err = art.GetArtForArtist(ctx, idParameter, sizeInt)
+			if err != nil {
+				logger.Printf("Error getting artist cover art for %s: %v", idParameter, err)
+				net.WriteSubsonicError(w, r, types.ErrorDataNotFound, "Cover art not found", "")
+				return
+			}
 		}
-		imageBlob, lastModified, err = art.GetArtForAlbum(ctx, albumId, sizeInt)
+	} else {
+		imageBlob, lastModified, err = art.GetArtForPodcast(ctx, idParameter, sizeInt)
 		if err != nil {
-			logger.Printf("Error getting album cover art for %s: %v", idParameter, err)
-			net.WriteSubsonicError(w, r, types.ErrorDataNotFound, "Cover art not found", "")
-			return
-		}
-	} else if metadataStruct.MusicbrainzAlbumId {
-		imageBlob, lastModified, err = art.GetArtForAlbum(ctx, idParameter, sizeInt)
-		if err != nil {
-			logger.Printf("Error getting album cover art for %s: %v", idParameter, err)
-			net.WriteSubsonicError(w, r, types.ErrorDataNotFound, "Cover art not found", "")
-			return
-		}
-	} else if metadataStruct.MusicbrainzArtistId {
-		imageBlob, lastModified, err = art.GetArtForArtist(ctx, idParameter, sizeInt)
-		if err != nil {
-			logger.Printf("Error getting artist cover art for %s: %v", idParameter, err)
+			logger.Printf("Error getting podcast cover art for %s: %v", idParameter, err)
 			net.WriteSubsonicError(w, r, types.ErrorDataNotFound, "Cover art not found", "")
 			return
 		}
