3b3a457c63240254c1e9bf78e06305c998538900
Ian Baron <ian.baron@manchester.ac.uk>
2025-10-04 18:30:14 +0100

    enable adding of custom art

--- frontend/src/components/ChangeAlbumArt.vue
+++ frontend/src/components/ChangeAlbumArt.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { SubsonicAlbum } from '../types/subsonicAlbum'
-import { fetchDeezerArtUrl, postNewAlbumArt } from '~/composables/backendFetch'
+import { fetchAlbumArtOptions, postNewAlbumArt } from '~/composables/backendFetch'
 
 const props = defineProps({
   album: { type: Object as PropType<SubsonicAlbum>, required: true },
@@ -10,23 +10,39 @@ const emits = defineEmits(['close', 'artUpdated'])
 
 const loading = ref(true)
 const deezerArtUrl = ref<string | null>(null)
+const coverArtArchiveUrl = ref<string | null>(null)
+const albumArt = ref<string | null>(null)
 
-async function getDeezerUrl() {
-  deezerArtUrl.value = await fetchDeezerArtUrl(props.album.albumArtists[0].name, props.album.name)
+async function getAlbumArtUrls() {
+  const options = await fetchAlbumArtOptions(props.album.albumArtists[0].name, props.album.name)
+  deezerArtUrl.value = options.deezer
+  coverArtArchiveUrl.value = options.cover_art_archive
 }
 
-async function updateArt() {
-  if (deezerArtUrl.value) {
-    const imageBlob = await (await fetch(deezerArtUrl.value)).blob()
+async function updateArt(source: 'deezer' | 'coverartarchive' | 'manual') {
+  let artUrl: string | null = null
+  switch (source) {
+    case 'deezer':
+      artUrl = deezerArtUrl.value
+      break
+    case 'coverartarchive':
+      artUrl = coverArtArchiveUrl.value
+      break
+    case 'manual':
+      artUrl = albumArt.value
+      break
+  }
+  if (artUrl) {
+    const imageBlob = await (await fetch(artUrl)).blob()
     const response = await postNewAlbumArt(props.album.id, imageBlob)
     if (response.status === 'ok') {
-      emits('artUpdated', deezerArtUrl.value)
+      emits('artUpdated', artUrl)
     }
   }
 }
 
 onMounted(async () => {
-  await getDeezerUrl()
+  await getAlbumArtUrls()
   loading.value = false
 })
 </script>
@@ -47,20 +63,51 @@ onMounted(async () => {
         <svg v-if="loading" class="text-primary" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
           <path fill="currentColor" d="M12 2A10 10 0 1 0 22 12A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8A8 8 0 0 1 12 20Z" opacity="0.5" /><path fill="currentColor" d="M20 12h2A10 10 0 0 0 12 2V4A8 8 0 0 1 20 12Z"><animateTransform attributeName="transform" dur="1s" from="0 12 12" repeatCount="indefinite" to="360 12 12" type="rotate" /></path>
         </svg>
-        <div v-if="deezerArtUrl" class="relative size-56">
-          <img
-            class="size-56"
-            :src="deezerArtUrl"
-            alt="Deezer Album Art"
-          />
-          <button
-            class="z-button absolute bottom-2 right-2"
-            aria-label="Choose art"
-            @click="updateArt()"
-          >
-            Use This Art
-          </button>
+        <div v-else class="flex flex-wrap justify-center gap-4">
+          <div v-if="deezerArtUrl" class="relative size-56">
+            <img
+              class="size-56"
+              :src="deezerArtUrl"
+              alt="Deezer Album Art"
+            />
+            <button
+              class="z-button absolute bottom-2 right-2"
+              aria-label="Choose art"
+              @click="updateArt('deezer')"
+            >
+              Use This Art
+            </button>
+          </div>
+          <div v-if="coverArtArchiveUrl" class="relative size-56">
+            <img
+              class="size-56"
+              :src="coverArtArchiveUrl"
+              alt="Cover Art Archive"
+            />
+            <button
+              class="z-button absolute bottom-2 right-2"
+              aria-label="Choose art"
+              @click="updateArt('coverartarchive')"
+            >
+              Use This Art
+            </button>
+          </div>
+          <div v-if="albumArt" class="relative size-56">
+            <img
+              class="size-56"
+              :src="albumArt"
+              alt="Album Art"
+            />
+            <button
+              class="z-button absolute bottom-2 right-2"
+              aria-label="Choose art"
+              @click="updateArt('manual')"
+            >
+              Use This Art
+            </button>
+          </div>
         </div>
+        <ImageSelector v-model="albumArt" />
       </div>
     </div>
   </teleport>
