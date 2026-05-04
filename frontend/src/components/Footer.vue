4c84f4a7d983d2877e826889860c60e642b2f3f8
Ian Baron <ian.baron@manchester.ac.uk>
2026-05-04 10:31:33 +0100

    remove @click.stop from trackinfo, fix previousIndexes logic for handlePreviousTrack()

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -12,12 +12,8 @@ const loggedIn = computed(() => {
     class="border-0 border-t-1 border-background-600 border-solid background-2 w-full bottom-0 sticky"
   >
     <div class="px-8 py-4 flex flex-col gap-2 items-center lg:flex-row lg:gap-6">
-      <div
-        class="flex flex-grow flex-col h-full w-full items-center justify-center"
-      >
-        <FooterTrack
-          class="mb-3 flex max-w-xs w-full items-center lg:max-w-200 sm:max-w-md lg:hidden"
-        />
+      <div class="flex flex-grow flex-col h-full w-full items-center justify-center">
+        <FooterTrack class="mb-3 flex max-w-xs w-full items-center lg:max-w-200 sm:max-w-md lg:hidden" />
         <PlayerProgressBar />
         <PlayerMediaControls />
       </div>
