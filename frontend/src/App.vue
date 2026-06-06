2d17913f3d506f14c02f97f270035a83313589b0
Ian Baron <ian.baron@manchester.ac.uk>
2026-06-06 10:29:26 +0100

    revert removal of css from RouterView component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,7 @@
     <PlayerAudio />
     <div class="flex flex-1 flex-col h-100svh min-h-0 overflow-x-hidden">
       <div class="p-2 flex flex-1 min-h-0 overflow-x-hidden overflow-y-auto lg:p-6 md:p-4">
-        <RouterView />
+        <RouterView class="flex-1 min-h-0 min-w-0" />
       </div>
       <Footer class="flex-shrink-0" />
     </div>
